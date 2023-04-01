5a2f41ec1112c9e77f8c167e0ff84b121a80a6dc
NoelKEL <noel.keller@stud.kftg.ch>
2023-04-01 13:28:04 +0200

    Database

--- Code/server/index.js
+++ Code/server/index.js
@@ -1,11 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const db = mysql.createConnection({})
+const db = mysql.createConnection({
+  user: "",
+  host: "",
+  password: "",
+  database: "",
+});
 /*
 Database connection...
 */
 
 app.listen(3001, () => {
-    console.log("Server is running")
+  console.log("Server is running");
 });
