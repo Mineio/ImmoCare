5b61d7b8af04ec0f578f44ab75e7f869be74a3c2
NoelKEL <noel.keller@stud.kftg.ch>
2023-04-16 14:15:34 +0200

    Update home.js

--- Code/react-website/src/pages/home.js
+++ Code/react-website/src/pages/home.js
@@ -1,43 +1,44 @@
 import React from "react";
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import Axios from "axios";
 
 const Home = () => {
-    const [property, setProperty] = useState([]);
+  const [property, setProperty] = useState([]);
 
-    useEffect(() => {
-        selectProperties();
-    }, []);
+  useEffect(() => {
+    selectProperties();
+  }, []);
 
-    const selectProperties = () => {
-        Axios.get("http://localhost:3001/getProperties").then((response) => {
-            setProperty(response.data);
-        });
-    };
+  const selectProperties = () => {
+    Axios.get("http://localhost:3001/getProperties").then((response) => {
+      setProperty(response.data);
+    });
+  };
 
-    return (
-        <div className="properties">
-            <div className="searchbar"> Searchbar</div>
-            <div className="listProperties">
-                <table>
-                    <tbody>
-                    {property.map((val, key) => {
-                        return (
-                            <tr key={key}>
-                                {/* Hier kommen noch die richtigen values heran */}
-                                <td id="">{val.name}</td>
-                                <td id="">{/*val.*/}</td>
-                                <td id="">{/*val.*/}</td>
-                                <td id="">{/*val.*/}</td>
-                                <td id="">{/*val.*/}</td>
-                            </tr>
-                        );
-                    })}
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    );
+  return (
+    <div className="properties">
+      <div className="searchbar"> Searchbar</div>
+      <div className="listProperties">
+        <table>
+          <tbody>
+            {property.map((val, key) => {
+              return (
+                <tr key={key}>
+                  {/* Hier kommen noch die richtigen values heran */}
+                  <td id="">{val.name}</td>
+                  <td id="">{/*val.*/}</td>
+                  <td id="">{/*val.*/}</td>
+                  <td id="">{/*val.*/}</td>
+                  <td id="">{/*val.*/}</td>
+                  <div class="berreich">Hallo Welt</div>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
