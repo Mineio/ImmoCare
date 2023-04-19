42618a482f1489e9370751c7fe97f8c26f8e90fb
Rahmschnitzel123 <104494588+Rahmschnitzel123@users.noreply.github.com>
2023-04-19 10:02:08 +0200

    227: 227: #3: Updated SQL-Script for merge

--- Documentation/ImmoCareScript.sql
+++ Documentation/ImmoCareScript.sql
@@ -4,65 +4,44 @@ Dieses Script erstellt die Datenbank, welche wir für unser ImmoCare Projekt ben
 Author: Noel Keller 
 Date:  30.3.23
 Version		Date		Who 	Changes
-1.0         30.3.23    Kel 	created
-Copyright © 2018  Noel Keller Kantonsschule Frauenfeld, Switzerland. All rights reserved
+1.0         30.3.23    	Kel 	created
+1.1			15.4.23	   	RV		removed TOrte, changed TUsers and TLiegenschaften
+Copyright © 2023  Noel Keller Kantonsschule Frauenfeld, Switzerland. All rights reserved
 ----------------------------------------------------------------*/
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
+
+USE SC_ImmoCareDB ;
+
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Table TUser
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+DROP TABLE IF EXISTS TUser ;
 
--- -----------------------------------------------------
--- Table `TOrte`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `TOrte` ;
-
-CREATE TABLE IF NOT EXISTS `TOrte` (
-  `OrtORNP` INT NOT NULL,
-  `OrtAdresse` VARCHAR(25) NOT NULL,
-  `OrtOrtschaft` VARCHAR(30) NOT NULL,
-  `OrtKanton` VARCHAR(30) NOT NULL,
-  PRIMARY KEY (`OrtORNP`))
+CREATE TABLE TUser (
+  UserID INT NOT NULL AUTO_INCREMENT,
+  UserEmail VARCHAR(30) NOT NULL,
+  UserPasswort VARCHAR(50) NOT NULL,
+  PRIMARY KEY (UserID))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `TImmobilienverwalter`
+-- Table TLiegenschaften
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `TImmobilienverwalter` ;
+DROP TABLE IF EXISTS TLiegenschaften ;
 
-CREATE TABLE IF NOT EXISTS `TImmobilienverwalter` (
-  `VerwId` INT NOT NULL AUTO_INCREMENT,
-  `VerwVorname` VARCHAR(15) NOT NULL,
-  `VerwNachname` VARCHAR(15) NOT NULL,
-  `VerwNr` VARCHAR(13) NOT NULL,
-  `VerwMail` VARCHAR(30) NOT NULL,
-  PRIMARY KEY (`VerwId`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `TImmobilien`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `TImmobilien` ;
-
-CREATE TABLE IF NOT EXISTS `TImmobilien` (
-  `ImmoId` INT NOT NULL AUTO_INCREMENT,
-  `ImmoGrundstückNr` VARCHAR(10) NOT NULL,
-  `ImmoBezeichnung` VARCHAR(150) NOT NULL,
-  `ImmoAusbaustandart` VARCHAR(45) NOT NULL,
-  `ImmoZustand` VARCHAR(45) NOT NULL,
-  `ImmoAnzahl` DECIMAL(3,1) NOT NULL,
-  `ImmoTyp` VARCHAR(25) NOT NULL,
-  `ImmoGesammtwert` VARCHAR(45) NOT NULL,
-  `TImmobilienverwalter_VerwId` INT NOT NULL,
-  `TOrte_OrtORNP` INT NOT NULL,
-  PRIMARY KEY (`ImmoId`))
+CREATE TABLE TLiegenschaften (
+  LiegNR CHAR(4) NOT NULL,
+  LiegTyp VARCHAR(30) NOT NULL,
+  LiegBezeichnung VARCHAR(100) NOT NULL,
+  LiegBaujahr Char(4) NOT NULL,
+  LiegGrundstückfläche INT UNSIGNED NOT NULL,
+  LiegNutzfläche INT UNSIGNED NOT NULL,
+  LiegAusbaustandart ENUM('einfach','normal','luxeriös') NOT NULL,
+  LiegZustand ENUM('normal','neuwertig','sanierungsbedürftig') NOT NULL,
+  LiegZusatz VARCHAR(50),
+  TUser_UserID INT NOT NULL,
+  PRIMARY KEY (LiegNR))
 ENGINE = InnoDB;
 
 
