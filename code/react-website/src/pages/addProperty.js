536193527bee2328409cd7c156781c925f9da659
NoelKEL <noel.keller@stud.kftg.ch>
2023-05-06 19:42:12 +0200

    LiegenschaftenHinzufügen.4
    Design Input forms

--- code/react-website/src/pages/addProperty.js
+++ code/react-website/src/pages/addProperty.js
@@ -3,35 +3,133 @@ import Axios from "axios";
 import { useState } from "react";
 
 function AddProperty() {
-  const [grundStückB, setgrundStückB] = useState("beliebig");
-  const [nutzFlB, setnutzFlB] = useState("beliebig");
+  const [Grundstückfläche, setGrundstückfläche] = useState("beliebig");
+  const [Nutzfläche, setNutzfläche] = useState("beliebig");
   const [ausbauSt, setausbauSt] = useState("beliebig");
   const [zustand, setzustand] = useState("beliebig");
-  const [chfVon, setchfVon] = useState("beliebig");
-  const [chfBis, setchfBis] = useState("beliebig");
-  const [baujahrVon, setbaujahrVon] = useState("beliebig");
-  const [Baujahrbis, setBaujahrbis] = useState("beliebig");
+  const [Preis, setPreis] = useState("beliebig");
+  const [Baujahr, setBaujahr] = useState("beliebig");
+  const [Liegenschaftstyp, setLiegenschaftstyp] = useState("beliebig");
 
   const insertProperties = () => {
     Axios.post("http://localhost:3001/insert", {
-      grundStück: grundStückB,
-      nutzfläche: nutzFlB,
+      grundStück: Grundstückfläche,
+      nutzfläche: Nutzfläche,
       ausbaustand: ausbauSt,
       zustandReq: zustand,
-      chfmin: chfVon,
-      chfmax: chfBis,
-      baujahrmin: baujahrVon,
-      baujahrmax: Baujahrbis,
+      chf: Preis,
+      baujahr: Baujahr,
     }).then((response) => {
       console.log(response);
     });
   };
 
   return (
-  <div className="App">
-
-  </div>;
-  )
+    <div className="App">
+      <div className="Container">
+        <div className="inputfields">
+          <div className="leftInputs">
+            <div className="left">
+              <div>
+                <form>
+                  <label htmlFor="wordsearch">Liegenschaftstyp</label>
+                  <select placeholder="" id="dropdown">
+                    <option value="" disabled selected hidden>
+                      Selektiere deine Option
+                    </option>
+                    <option>Einfamilienhaus</option>
+                    <option>Mehrfamilienhaus</option>
+                    <option>Gebwerbeliegenschaft</option>
+                  </select>
+                </form>
+              </div>
+              <div>
+                <form>
+                  <label>Grundstückfläche</label>
+                  <input
+                    type="text"
+                    placeholder="Eingabe"
+                    onChange={(e) => setGrundstückfläche(e.target.value)}
+                  ></input>
+                </form>
+              </div>
+              <div>
+                <form>
+                  <label htmlFor="nutzFlB">Nutzfläche bis</label>
+                  <input
+                    type="text"
+                    placeholder="Eingabe"
+                    onChange={(e) => setNutzfläche(e.target.value)}
+                  ></input>
+                </form>
+              </div>
+              <div>
+                <form>
+                  <label htmlFor="ausbauSt">Ausbaustand</label>
+                  <select placeholder="" id="dropdown">
+                    <option value="" disabled selected hidden>
+                      Selektiere deine Option
+                    </option>
+                    <option>rustikal</option>
+                    <option>normal</option>
+                    <option>einfach</option>
+                    <option>luxeriös</option>
+                  </select>
+                </form>
+              </div>
+            </div>
+            <div>
+              <form>
+                <label htmlFor="Zustand">Zustand</label>
+                <select placeholder="" id="dropdown">
+                  <option value="" disabled selected hidden>
+                    Selektiere deine Option
+                  </option>
+                  <option>neuwertig</option>
+                  <option>normal</option>
+                  <option>renoviert</option>
+                  <option>sanierungsbedürftig</option>
+                  <option>Gebwerbeliegenschaft</option>
+                  <option>Gebwerbeliegenschaft</option>
+                </select>
+              </form>
+            </div>
+            <div>
+              <form>
+                <label htmlFor="preis">Preis</label>
+                <input
+                  type="text"
+                  placeholder="Eingabe"
+                  onChange={(e) => setPreis(e.target.value)}
+                ></input>
+              </form>
+            </div>
+            <div>
+              <form>
+                <label htmlFor="Baujahr">Baujahr</label>
+                <input
+                  type="text"
+                  placeholder="Eingabe"
+                  onChange={(e) => setBaujahr(e.target.value)}
+                ></input>
+              </form>
+            </div>
+            <div>
+              <form>
+                <label htmlFor="preis">Preis</label>
+                <input
+                  type="text"
+                  placeholder="Eingabe"
+                  onChange={(e) => setPreis(e.target.value)}
+                ></input>
+              </form>
+            </div>
+            <button>Hinzufügen</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AddProperty;
